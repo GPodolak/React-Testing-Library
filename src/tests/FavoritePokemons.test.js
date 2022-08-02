c27e67e75e433d2cf63b1659be4748a1fface50f
Guilherme Podolak <podolak12345@gmail.com>
2022-08-02 17:12:11 -0300

    reqisito 3 atualizado

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import FavoritePokemons from '../components/Pokemon';
 import renderWithRouter from './Aux/RenderWithRouter';
+import App from '../App';
 
-describe('testando ocomponente FavoritePokemon', () => {
-  test('teste a exibição damensagem "No favorite pokemon found"', () => {
+describe('Teste o componente Favorite Pokemons', () => {
+  test('Teste se é exibido na tela a mensagem No favorite Pokemon found', () => {
     renderWithRouter(<FavoritePokemons />);
+    const pokeMessage = screen.getByText(/No favorite Pokemon found/i);
+    expect(pokeMessage).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokemons favoritados', () => {
+    renderWithRouter(<App />);
+    const pokeDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(pokeDetails);
+
+    const favoriteCard = screen.getByLabelText(/Pokémon favoritado?/i);
+    userEvent.click(favoriteCard);
+
+    const favoritePokemon = screen.getByText(/Favorite Pokémons/i);
+    userEvent.click(favoritePokemon);
+
+    const pikachu = screen.getByRole('img', { name: /pikachu sprite/i });
+    expect(pikachu).toBeInTheDocument();
   });
 });
