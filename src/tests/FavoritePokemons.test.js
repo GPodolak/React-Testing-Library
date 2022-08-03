49874ea08964ce6484f08931de5ad805b45f0570
Guilherme Podolak <podolak12345@gmail.com>
2022-08-02 22:04:10 -0300

    requisito not found agr vai

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import FavoritePokemons from '../components/FavoritePokemons';
+import FavoritePokemons from '../components/Pokemon';
 import renderWithRouter from './Aux/RenderWithRouter';
 import App from '../App';
 
