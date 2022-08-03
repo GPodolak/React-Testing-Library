5c83bf67a208aadf9e0b6af223e89d5ebfbb60df
Guilherme Podolak <podolak12345@gmail.com>
2022-08-03 17:10:25 -0300

    pedaço do pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './Aux/RenderWithRouter';
+
+describe('teste o componente Pokedex', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  test('testa o texto na pagina', () => {
+    const poketext = screen.getByRole('heading', { level: 2 });
+    expect(poketext).toBeInTheDocument();
+  });
+});
